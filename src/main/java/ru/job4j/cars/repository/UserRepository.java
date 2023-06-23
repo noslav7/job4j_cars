65cd3ca6e1b9535e4716a78ccc62fac3a0ae51e2
noslav7 <noslav7@gmail.com>
2023-06-23 15:56:35 +0500

    2. HQL [#6874 # [#6874]]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ru.job4j.cars.repository;
 
 import lombok.AllArgsConstructor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import ru.job4j.cars.model.User;
 
@@ -13,60 +14,135 @@ public class UserRepository {
 
     /**
      * Сохранить в базе.
+     *
      * @param user пользователь.
      * @return пользователь с id.
      */
     public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
         return user;
     }
 
     /**
      * Обновить в базе пользователя.
+     *
      * @param user пользователь.
      */
     public void update(User user) {
-
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
     }
 
     /**
      * Удалить пользователя по id.
+     *
      * @param userId ID
      */
     public void delete(int userId) {
-
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.delete(findById(userId));
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
     }
 
     /**
      * Список пользователь отсортированных по id.
+     *
      * @return список пользователей.
      */
     public List<User> findAllOrderById() {
-        return List.of();
+        Session session = sf.openSession();
+        List result = null;
+        try {
+            session.beginTransaction();
+            result = session.createQuery(
+                    "from ru.job4j.cars.model.User user ORDER BY user.id Asc").list();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return result;
     }
 
     /**
      * Найти пользователя по ID
+     *
      * @return пользователь.
      */
     public Optional<User> findById(int userId) {
-        return Optional.empty();
+        Session session = sf.openSession();
+        User result = null;
+        try {
+            session.beginTransaction();
+            result = session.get(User.class, userId);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return Optional.of(result);
     }
 
     /**
      * Список пользователей по login LIKE %key%
+     *
      * @param key key
      * @return список пользователей.
      */
     public List<User> findByLikeLogin(String key) {
-        return List.of();
+        Session session = sf.openSession();
+        List<User> result = null;
+        try {
+            session.beginTransaction();
+            session.createQuery("from ru.job4j.cars.model.User user where user.login = :k")
+                    .setParameter("k", key)
+                    .executeUpdate();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return result;
     }
 
     /**
      * Найти пользователя по login.
+     *
      * @param login login.
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
-        return Optional.empty();
+        Session session = sf.openSession();
+        User result = null;
+        try {
+            session.beginTransaction();
+            result = session.get(User.class, login);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return Optional.of(result);
     }
 }
